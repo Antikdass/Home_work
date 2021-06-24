8823ecf46c8cc36ba3dbcb1544bca14a016476d4
Antikdass <54902898+Antikdass@users.noreply.github.com>
2021-06-24 22:34:47 +0300

    Delete untitled/src/characters directory

--- untitled/src/characters/CharactersComponent.js
+++
@@ -1,9 +0,0 @@
-import './CharactersComponent.css';
-function CharactersComponent(props) {
- return   <div className="App">
-     <img src={props.img} alt=""/>
-     <p>
-         {props.text}
-     </p>
-    </div>
-}export default CharactersComponent;
